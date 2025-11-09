7573a4207b0b586578e4b85dac601983466c8a53
barkatah75-jpg <barkatah75@gmail.com>
2025-11-09 23:37:56 +0530

    Initial commit for AIVANA Backend

---
+++ server.js
@@ -0,0 +1,8 @@
+﻿const express = require('express');
+const app = express();
+let port = 5000;
+function startServer(p){
+  app.get('/', (req, res) => res.send('AIVANA Backend API running on port ' + p));
+  app.listen(p, () => console.log('ðŸš€ Backend active on port', p));
+}
+startServer(port);
